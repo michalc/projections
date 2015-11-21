06c9689eca3b57da6a2acd8a7d65f9d6da8cab79
Michal Charemza <michalcharemza@gmail.com>
2015-11-21 14:25:21 +0000

    First attempt at adjusting lat long offsets

--- src/app.js
+++ src/app.js
@@ -9,6 +9,16 @@
     return $window.Mercator;
   });
 
+  app.directive('ngModelInt', function() {
+    return {
+      require: 'ngModel',
+      link: function(scope, element, attrs, ngModelController) {
+        ngModelController.$parsers.push(function(value) {
+          return parseInt(value);
+        });
+      }
+    };
+  });
 
   app.directive('chart', function(Mercator) {
     return {
@@ -24,16 +34,28 @@
       },
       link: function(scope, element, attrs) {
         var svg = element.find('svg');
+        var g = null;
         var bounds = scope.$eval(attrs.chartBounds);
 
-        scope.$watch(attrs.chart, function(chart) {
-          if (!chart) return;
+        // Would it be faster to edit the existing one?
+        function createChart(chart, offsetLongitude, offsetLatitude) {
+          if (!chart || typeof offsetLongitude != 'number' || typeof offsetLatitude != 'number')  return;
+          if (g) {
+            g.remove();
+            g = null;
+          }
           var t1 = performance.now();
           var path = '<svg xmlns="http://www.w3.org/2000/svg" version="1.1"><g>';
           chart.features.forEach(function(feature) {
             var chartCoords = [];
             feature.geometry.coordinates[0].forEach(function(longLat) {
-              chartCoords.push(Mercator.toChart(bounds, longLat[0], longLat[1]));
+              // *Very* broken since the shapes cross the entire map
+              // would need to do something clever like copy the entire shape to the other side
+              var offsetLong = longLat[0] + offsetLongitude;
+              offsetLong = offsetLong < -180 ? offsetLong + 360 : (offsetLong > 180 ? offsetLong - 360 : offsetLong);
+              var offsetLat = longLat[1] + offsetLatitude;
+              offsetLat = offsetLat < -90 ? offsetLat + 180 : (offsetLat > 90 ? offsetLat - 180 : offsetLat);
+              chartCoords.push(Mercator.toChart(bounds, offsetLong, offsetLat));
             });
 
             path += '<path class="land" d="';
@@ -46,10 +68,18 @@
           var t2 = performance.now();
           console.info(Math.round((t2 - t1) * 1000) / 1000, 'milliseconds to create text SVG element');
           var content = new DOMParser().parseFromString(path, 'text/xml');
-          var g = content.children[0].children[0];
+          g = content.children[0].children[0];
           svg.append(g);
           var t3 = performance.now();
           console.info(Math.round((t3 - t2) * 1000) / 1000, 'milliseconds to append to document');
+        }
+
+        scope.$watchGroup([attrs.chart, function() {
+          return scope.$eval(attrs.offsets).longitude;
+        }, function() {
+          return scope.$eval(attrs.offsets).latitude;
+        }], function(group) {
+          createChart(group[0], group[1], group[2]);
         });
       }
     };
@@ -60,6 +90,11 @@
     $http.get('data/GSHHS_c_L1.json').then(function(results) {
       $scope.chart = results.data;
     });
+
+    $scope.offsets = {
+      longitude: 0,
+      latitude: 0
+    };
 
     $scope.bounds = {
       earth: {
