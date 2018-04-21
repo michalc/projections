611ef2703c63d603c438d4fefda5c63a665dffb2
Michal Charemza <michal@charemza.name>
2018-04-21 21:32:34 +0100

    Bump shrinkwrapped versions

--- gulpfile.js
+++ gulpfile.js
@@ -63,7 +63,7 @@ gulp.task('default', [], function() {
     .pipe(gulp.dest(dataDest));
 
   var jsSrc = [
-    'node_modules/lodash/dist/lodash.min.js',
+    'node_modules/lodash/lodash.min.js',
     'node_modules/angular/angular.min.js',
     'src/mercator.js',
     'src/app.js'
