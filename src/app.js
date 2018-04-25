6a1bc71eea875e127cedf444e0c29301285ae7f3
Michal Charemza <michal@charemza.name>
2018-04-25 12:09:24 +0100

    No need for extra closure since using browserify

--- src/app.js
+++ src/app.js
@@ -1,104 +1,102 @@
 /* eslint-env browser */
 
-(function() {
-  'use strict';
+'use strict';
 
-  var Mercator = require('./mercator');
+var Mercator = require('./mercator');
 
-  function fetch(url) {
-    return new Promise(function(resolve, reject) {
-      function reqListener() {
-        resolve(JSON.parse(oReq.responseText));
-      }
-
-      var oReq = new XMLHttpRequest();
-      oReq.addEventListener('load', reqListener);
-      oReq.open('GET', url);
-      oReq.send(); 
-    });
-  }
-
-  var pathPool = [];
-  var rotatedCoords;
-  function initCharts(charts, svg) {
-    var maxLength = -Infinity;
-    for (var i = 0; i < charts.length; ++i) {
-      maxLength = Math.max(charts[i].length, maxLength);
-
-      var pathElement = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-      pathElement.setAttributeNS(null, 'class', 'land');
-      pathPool.push(pathElement);
-      svg.appendChild(pathElement);
-    }
-    rotatedCoords = new Float64Array(8 * 2 * maxLength);
-  }
-    
-  function createChart(svg, charts, bounds, offsetLongitude, offsetLatitude) {
-    for (var j = 0; j < charts.length; ++j) {
-      // Fill rotatedCoords
-      for (var i = 0; i < charts[j].length; ++i) {
-        Mercator.rotate(offsetLongitude, offsetLatitude, charts[j][i], rotatedCoords, i*2);
-      }
-
-      var shape = Mercator.getShape(bounds, charts[j].length, rotatedCoords);
-      pathPool[j].setAttributeNS(null, 'd', shape);
-    }
-  }
-
-  window.addEventListener('load', function() {
-    var bounds = {
-      earth: {
-        top: 83.6,
-        bottom: -83.6,
-        left: -180,
-        right: 180
-      },
-      screen: {
-        top: 0,
-        bottom: 740,
-        left: 0,
-        right: 800
-      }
-    };
-
-    var latitudeInput = document.getElementById('latitude-input');
-    var longitudeInput = document.getElementById('longitude-input');
-
-    var latitude = parseFloat(latitudeInput.value);
-    latitudeInput.addEventListener('input', function() {
-      latitude = parseFloat(latitudeInput.value);
-      draw();
-    });
-
-    var longitude = parseFloat(longitudeInput.value);
-    longitudeInput.addEventListener('input', function() {
-      longitude = parseFloat(longitudeInput.value);
-      draw();
-    });
-
-    var svg = document.getElementById('svg');
-    function setSvgDimensions() {
-      var screenBound = Math.min(window.innerWidth, window.innerHeight - 40);
-      svg.setAttribute('width', screenBound);
-      svg.setAttribute('height', screenBound);
-      bounds.screen.right = screenBound;
-      bounds.screen.bottom = screenBound;
-    }
-    window.addEventListener('resize', setSvgDimensions);
-    window.addEventListener('resize', draw);
-    setSvgDimensions();
-
-    var charts;
-    function draw() {
-      if (!charts) return;
-      createChart(svg, charts, bounds, longitude, latitude);
+function fetch(url) {
+  return new Promise(function(resolve, reject) {
+    function reqListener() {
+      resolve(JSON.parse(oReq.responseText));
     }
 
-    fetch('data/data.json').then(function(results) {
-      charts = results;
-      initCharts(charts, svg);
-      draw();
-      document.body.removeAttribute('class');
-    });
+    var oReq = new XMLHttpRequest();
+    oReq.addEventListener('load', reqListener);
+    oReq.open('GET', url);
+    oReq.send();
   });
-})();
+}
+
+var pathPool = [];
+var rotatedCoords;
+function initCharts(charts, svg) {
+  var maxLength = -Infinity;
+  for (var i = 0; i < charts.length; ++i) {
+    maxLength = Math.max(charts[i].length, maxLength);
+
+    var pathElement = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    pathElement.setAttributeNS(null, 'class', 'land');
+    pathPool.push(pathElement);
+    svg.appendChild(pathElement);
+  }
+  rotatedCoords = new Float64Array(8 * 2 * maxLength);
+}
+
+function createChart(svg, charts, bounds, offsetLongitude, offsetLatitude) {
+  for (var j = 0; j < charts.length; ++j) {
+    // Fill rotatedCoords
+    for (var i = 0; i < charts[j].length; ++i) {
+      Mercator.rotate(offsetLongitude, offsetLatitude, charts[j][i], rotatedCoords, i*2);
+    }
+
+    var shape = Mercator.getShape(bounds, charts[j].length, rotatedCoords);
+    pathPool[j].setAttributeNS(null, 'd', shape);
+  }
+}
+
+window.addEventListener('load', function() {
+  var bounds = {
+    earth: {
+      top: 83.6,
+      bottom: -83.6,
+      left: -180,
+      right: 180
+    },
+    screen: {
+      top: 0,
+      bottom: 740,
+      left: 0,
+      right: 800
+    }
+  };
+
+  var latitudeInput = document.getElementById('latitude-input');
+  var longitudeInput = document.getElementById('longitude-input');
+
+  var latitude = parseFloat(latitudeInput.value);
+  latitudeInput.addEventListener('input', function() {
+    latitude = parseFloat(latitudeInput.value);
+    draw();
+  });
+
+  var longitude = parseFloat(longitudeInput.value);
+  longitudeInput.addEventListener('input', function() {
+    longitude = parseFloat(longitudeInput.value);
+    draw();
+  });
+
+  var svg = document.getElementById('svg');
+  function setSvgDimensions() {
+    var screenBound = Math.min(window.innerWidth, window.innerHeight - 40);
+    svg.setAttribute('width', screenBound);
+    svg.setAttribute('height', screenBound);
+    bounds.screen.right = screenBound;
+    bounds.screen.bottom = screenBound;
+  }
+  window.addEventListener('resize', setSvgDimensions);
+  window.addEventListener('resize', draw);
+  setSvgDimensions();
+
+  var charts;
+  function draw() {
+    if (!charts) return;
+    createChart(svg, charts, bounds, longitude, latitude);
+  }
+
+  fetch('data/data.json').then(function(results) {
+    charts = results;
+    initCharts(charts, svg);
+    draw();
+    document.body.removeAttribute('class');
+  });
+});
--- src/mercator.js
+++ src/mercator.js
@@ -1,179 +1,179 @@
 /* global exports */
 
-(function(Mercator) {
-  'use strict';
+'use strict';
 
-  Mercator.toChart = toChart;
-  Mercator.rotate = rotate;
-  Mercator.getShape = getShape;
+var Mercator = module.exports;
 
-  // Points at infinity on the chart
-  // get mapped to this
-  var MAX_BOUND = 99999;
+Mercator.toChart = toChart;
+Mercator.rotate = rotate;
+Mercator.getShape = getShape;
 
-  function toRadians(deg) {
-    return deg * Math.PI / 180;
+// Points at infinity on the chart
+// get mapped to this
+var MAX_BOUND = 99999;
+
+function toRadians(deg) {
+  return deg * Math.PI / 180;
+}
+
+function toDegrees(rad) {
+  return rad * 180 / Math.PI;
+}
+
+function thetaToY(W, theta) {
+  // Fudge to be able to plot things at (/beyond) pole
+  // this is useful since shapes might contain vertices
+  // that are at infinity, but still want to plot the ones
+  // that aren't
+  if (theta >= Math.PI / 2) return MAX_BOUND;
+  if (theta <= -Math.PI / 2) return -MAX_BOUND;
+  return W / (2 * Math.PI) * Math.log(Math.tan(Math.PI / 4 + theta / 2));
+}
+
+function xToLambda(W, lambda_0, x) {
+  return lambda_0 + x * 2 * Math.PI / W; 
+}
+
+function lambdaToX(W, lambda_0, lambda) {
+  return W / (2 * Math.PI) * (lambda - lambda_0);
+}
+
+function getY_top(W, chartBounds) {
+  var theta_top = toRadians(chartBounds.earth.top);
+  return thetaToY(W, theta_top);
+}
+
+function toChart(chartBounds, long, lat, out) {
+  var W = chartBounds.screen.right - chartBounds.screen.left;
+
+  var theta = toRadians(lat);
+  var y = thetaToY(W, theta);
+  var y_top = getY_top(W, chartBounds);
+  var chartY = y_top - y;
+
+  var lambda = toRadians(long);
+  var lambda_0 = toRadians(chartBounds.earth.left);
+  var x = lambdaToX(W, lambda_0, lambda);
+  var chartX = x;
+
+  out[0] = Math.trunc(chartX);
+  out[1] = Math.trunc(chartY);
+}
+
+// latRotation rotates about y axis (line through earth along original equator)
+// longRotation rotates about z axis (line through earth pole to pole)
+function rotate(longRotationDegrees, latRotationDegrees, longLat, resultArray, resultOffset) {
+  var long = longLat[0];
+  var lat = longLat[1];
+
+  // Convert to spherical-polar radian coordinates
+  var theeta = toRadians(long);
+  var phi = toRadians(lat + 90); // In usual spherical-polar coords, phi is 0 along z-axis
+
+  // Convert to cartesian coordinates (assuming radius of Earth is 1)
+  // http://mathworld.wolfram.com/SphericalCoordinates.html
+  var sinPhi = Math.sin(phi)
+  var x = Math.cos(theeta) * sinPhi;
+  var y = Math.sin(theeta) * sinPhi;
+  var z = Math.cos(phi);
+
+  // Convert rotation angle to radians
+  var rotLong = toRadians(longRotationDegrees);
+  var rotLat = toRadians(latRotationDegrees);
+
+  // Rotate about z axis
+  var sinRotLong = Math.sin(rotLong);
+  var cosRotLong = Math.cos(rotLong);
+  var x_r1 = x * cosRotLong - y * sinRotLong;
+  var y_r1 = x * sinRotLong + y * cosRotLong;
+  var z_r1 = z;
+
+  // Rotate about y axis
+  var sinRotLat = Math.sin(rotLat);
+  var cosRotLat = Math.cos(rotLat);
+  var x_r2 = x_r1 * cosRotLat + z_r1 * sinRotLat;
+  var y_r2 = y_r1;
+  var z_r2 = z_r1 * cosRotLat - x_r1 * sinRotLat;
+
+  // +ve / 0 = Infinity, -ve / 0 = -Infinity, and
+  // atan works for +- Infinity, but, 0 / 0 = NaN,
+  // so we have to handle that case
+  var theeta_r2 = x_r2 != 0 || y_r2 != 0 ?  Math.atan(y_r2 / x_r2) : 
+                     Object.is(y_r2, -0) ? -Math.PI / 2 :
+                                            Math.PI / 2;
+
+  var phi_r2 = Math.acos(z_r2);
+
+  // Convert to lat/long
+  var long_r2 = toDegrees(theeta_r2);
+  if      (x_r2 < 0 && y_r2 <= 0) long_r2 = long_r2 - 180;
+  else if (x_r2 < 0 && y_r2 >= 0) long_r2 = long_r2 + 180;
+  var lat_r2 = toDegrees(phi_r2) - 90;
+
+  resultArray[resultOffset] = long_r2;
+  resultArray[resultOffset + 1] = lat_r2;
+}
+
+// Fudge to determine is 2 points are discontinuous
+var DISCONTINUTY_THREASHOLD = 180;
+
+// 1 for -180 to 180, -1 for 180 to -180
+function discontinuityDirection(prev, curr) {
+  return Math.abs(prev - curr) > DISCONTINUTY_THREASHOLD && prev * curr < 0 ? (prev < curr ? 1 : -1) : 0;
+}
+
+function prev(length, i) {
+  return i == 0 ? length - 1 : i - 1;
+}
+
+var tempCoords = new Float64Array(8 * 2);
+function getShape(bounds, numCoords, rotatedCoords) {
+  // Fairly performance critical
+
+  var minLat = Infinity;
+  var maxLat = -Infinity;
+  for (var i = 0; i < numCoords; ++i) {
+    minLat = Math.min(rotatedCoords[i*2+1], minLat);
+    maxLat = Math.max(rotatedCoords[i*2+1], maxLat);
   }
 
-  function toDegrees(rad) {
-    return rad * 180 / Math.PI;
-  }
+  // Slight hack: pole is determined by the point closest
+  var latDiffToSouthPole = Math.abs(-90 - minLat);
+  var latDiffToNorthPole = Math.abs(90 - maxLat);
+  var pole = latDiffToSouthPole <= latDiffToNorthPole ? -1 : 1;
+  var offLat = 88;
+  var extraLong = 10;
 
-  function thetaToY(W, theta) {
-    // Fudge to be able to plot things at (/beyond) pole
-    // this is useful since shapes might contain vertices
-    // that are at infinity, but still want to plot the ones
-    // that aren't
-    if (theta >= Math.PI / 2) return MAX_BOUND;
-    if (theta <= -Math.PI / 2) return -MAX_BOUND;
-    return W / (2 * Math.PI) * Math.log(Math.tan(Math.PI / 4 + theta / 2));
-  }
-
-  function xToLambda(W, lambda_0, x) {
-    return lambda_0 + x * 2 * Math.PI / W; 
-  }
-
-  function lambdaToX(W, lambda_0, lambda) {
-    return W / (2 * Math.PI) * (lambda - lambda_0);
-  }
-
-  function getY_top(W, chartBounds) {
-    var theta_top = toRadians(chartBounds.earth.top);
-    return thetaToY(W, theta_top);
-  }
-
-  function toChart(chartBounds, long, lat, out) {
-    var W = chartBounds.screen.right - chartBounds.screen.left;
-
-    var theta = toRadians(lat);
-    var y = thetaToY(W, theta);
-    var y_top = getY_top(W, chartBounds);
-    var chartY = y_top - y;
-
-    var lambda = toRadians(long);
-    var lambda_0 = toRadians(chartBounds.earth.left);
-    var x = lambdaToX(W, lambda_0, lambda);
-    var chartX = x;
-
-    out[0] = Math.trunc(chartX);
-    out[1] = Math.trunc(chartY);
-  }
-
-  // latRotation rotates about y axis (line through earth along original equator)
-  // longRotation rotates about z axis (line through earth pole to pole)
-  function rotate(longRotationDegrees, latRotationDegrees, longLat, resultArray, resultOffset) {
-    var long = longLat[0];
-    var lat = longLat[1];
-
-    // Convert to spherical-polar radian coordinates
-    var theeta = toRadians(long);
-    var phi = toRadians(lat + 90); // In usual spherical-polar coords, phi is 0 along z-axis
-
-    // Convert to cartesian coordinates (assuming radius of Earth is 1)
-    // http://mathworld.wolfram.com/SphericalCoordinates.html
-    var sinPhi = Math.sin(phi)
-    var x = Math.cos(theeta) * sinPhi;
-    var y = Math.sin(theeta) * sinPhi;
-    var z = Math.cos(phi);
-
-    // Convert rotation angle to radians
-    var rotLong = toRadians(longRotationDegrees);
-    var rotLat = toRadians(latRotationDegrees);
-
-    // Rotate about z axis
-    var sinRotLong = Math.sin(rotLong);
-    var cosRotLong = Math.cos(rotLong);
-    var x_r1 = x * cosRotLong - y * sinRotLong;
-    var y_r1 = x * sinRotLong + y * cosRotLong;
-    var z_r1 = z;
-
-    // Rotate about y axis
-    var sinRotLat = Math.sin(rotLat);
-    var cosRotLat = Math.cos(rotLat);
-    var x_r2 = x_r1 * cosRotLat + z_r1 * sinRotLat;
-    var y_r2 = y_r1;
-    var z_r2 = z_r1 * cosRotLat - x_r1 * sinRotLat;
-
-    // +ve / 0 = Infinity, -ve / 0 = -Infinity, and
-    // atan works for +- Infinity, but, 0 / 0 = NaN,
-    // so we have to handle that case
-    var theeta_r2 = x_r2 != 0 || y_r2 != 0 ?  Math.atan(y_r2 / x_r2) : 
-                       Object.is(y_r2, -0) ? -Math.PI / 2 :
-                                              Math.PI / 2;
-
-    var phi_r2 = Math.acos(z_r2);
-
-    // Convert to lat/long
-    var long_r2 = toDegrees(theeta_r2);
-    if      (x_r2 < 0 && y_r2 <= 0) long_r2 = long_r2 - 180;
-    else if (x_r2 < 0 && y_r2 >= 0) long_r2 = long_r2 + 180;
-    var lat_r2 = toDegrees(phi_r2) - 90;
-
-    resultArray[resultOffset] = long_r2;
-    resultArray[resultOffset + 1] = lat_r2;
-  }
-
-  // Fudge to determine is 2 points are discontinuous
-  var DISCONTINUTY_THREASHOLD = 180;
-
-  // 1 for -180 to 180, -1 for 180 to -180
-  function discontinuityDirection(prev, curr) {
-    return Math.abs(prev - curr) > DISCONTINUTY_THREASHOLD && prev * curr < 0 ? (prev < curr ? 1 : -1) : 0;
-  }
-
-  function prev(length, i) {
-    return i == 0 ? length - 1 : i - 1;
-  }
-
-  var tempCoords = new Float64Array(8 * 2);
-  function getShape(bounds, numCoords, rotatedCoords) {
-    // Fairly performance critical
-
-    var minLat = Infinity;
-    var maxLat = -Infinity;
-    for (var i = 0; i < numCoords; ++i) {
-      minLat = Math.min(rotatedCoords[i*2+1], minLat);
-      maxLat = Math.max(rotatedCoords[i*2+1], maxLat);
+  var shape = ''
+  for (var i = 0; i < numCoords; ++i) {
+    var currLong = rotatedCoords[i*2];
+    var currLat = rotatedCoords[i*2+1];
+    var prevIndex = prev(numCoords, i);
+    var prevLong = rotatedCoords[prevIndex*2];
+    var prevLat = rotatedCoords[prevIndex*2+1];
+    var direction = discontinuityDirection(prevLong, currLong);
+    if (direction) {
+      Mercator.toChart(bounds, currLong - 360 * direction, currLat, tempCoords);
+      shape += (i == 0 ? 'M' : 'L') + tempCoords[0] + ',' + tempCoords[1]
+      Mercator.toChart(bounds, currLong - 360 * direction, currLat, tempCoords)
+      shape += 'L' + tempCoords[0] + ',' + tempCoords[1]
+      Mercator.toChart(bounds, currLong - (360 + extraLong) * direction, currLat, tempCoords);
+      shape += 'L' + tempCoords[0] + ',' + tempCoords[1]
+      Mercator.toChart(bounds, currLong - (360 + extraLong) * direction, offLat * pole, tempCoords);
+      shape += 'L' + tempCoords[0] + ',' + tempCoords[1]
+      Mercator.toChart(bounds, prevLong + (360 + extraLong) * direction, offLat * pole, tempCoords);
+      shape += 'L' + tempCoords[0] + ',' + tempCoords[1]
+      Mercator.toChart(bounds, prevLong + (360 + extraLong) * direction, prevLat, tempCoords);
+      shape += 'L' + tempCoords[0] + ',' + tempCoords[1]
+      Mercator.toChart(bounds, prevLong + 360 * direction, prevLat, tempCoords);
+      shape += 'L' + tempCoords[0] + ',' + tempCoords[1]
+      Mercator.toChart(bounds, currLong, currLat, tempCoords);
+      shape += 'L' + tempCoords[0] + ',' + tempCoords[1]
+    } else {
+      Mercator.toChart(bounds, currLong, currLat, tempCoords);
+      shape += (i == 0 ? 'M' : 'L') + tempCoords[0] + ',' + tempCoords[1]    
     }
-
-    // Slight hack: pole is determined by the point closest
-    var latDiffToSouthPole = Math.abs(-90 - minLat);
-    var latDiffToNorthPole = Math.abs(90 - maxLat);
-    var pole = latDiffToSouthPole <= latDiffToNorthPole ? -1 : 1;
-    var offLat = 88;
-    var extraLong = 10;
-
-    var shape = ''
-    for (var i = 0; i < numCoords; ++i) {
-      var currLong = rotatedCoords[i*2];
-      var currLat = rotatedCoords[i*2+1];
-      var prevIndex = prev(numCoords, i);
-      var prevLong = rotatedCoords[prevIndex*2];
-      var prevLat = rotatedCoords[prevIndex*2+1];
-      var direction = discontinuityDirection(prevLong, currLong);
-      if (direction) {
-        Mercator.toChart(bounds, currLong - 360 * direction, currLat, tempCoords);
-        shape += (i == 0 ? 'M' : 'L') + tempCoords[0] + ',' + tempCoords[1]
-        Mercator.toChart(bounds, currLong - 360 * direction, currLat, tempCoords)
-        shape += 'L' + tempCoords[0] + ',' + tempCoords[1]
-        Mercator.toChart(bounds, currLong - (360 + extraLong) * direction, currLat, tempCoords);
-        shape += 'L' + tempCoords[0] + ',' + tempCoords[1]
-        Mercator.toChart(bounds, currLong - (360 + extraLong) * direction, offLat * pole, tempCoords);
-        shape += 'L' + tempCoords[0] + ',' + tempCoords[1]
-        Mercator.toChart(bounds, prevLong + (360 + extraLong) * direction, offLat * pole, tempCoords);
-        shape += 'L' + tempCoords[0] + ',' + tempCoords[1]
-        Mercator.toChart(bounds, prevLong + (360 + extraLong) * direction, prevLat, tempCoords);
-        shape += 'L' + tempCoords[0] + ',' + tempCoords[1]
-        Mercator.toChart(bounds, prevLong + 360 * direction, prevLat, tempCoords);
-        shape += 'L' + tempCoords[0] + ',' + tempCoords[1]
-        Mercator.toChart(bounds, currLong, currLat, tempCoords);
-        shape += 'L' + tempCoords[0] + ',' + tempCoords[1]
-      } else {
-        Mercator.toChart(bounds, currLong, currLat, tempCoords);
-        shape += (i == 0 ? 'M' : 'L') + tempCoords[0] + ',' + tempCoords[1]    
-      }
-    }
-
-    return shape + 'z';
   }
-})(module.exports);
+
+  return shape + 'z';
+}
